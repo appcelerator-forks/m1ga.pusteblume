a33385a9a8bd07924cc8f3f2679e88dac120dc92
m1ga <migamiga@web.de>
2015-02-07 14:20:25 +0100

    more markdown

--- app/alloy.js
+++ app/alloy.js
@@ -18,7 +18,7 @@ Alloy.Globals.formatDate = function(date) {
     var h = dat.getHours();
     var m = dat.getMinutes();
     var d = dat.getDate();
-    var mon = dat.getMonth()+1;
+    var mon = dat.getMonth() + 1;
     if (h < 10)
         h = "0" + h;
     if (m < 10)
@@ -37,11 +37,42 @@ Alloy.Globals.replaceText = function(txt) {
     txt = txt.replace(match, "$1");
 
     //replace links
-    match = /\[(.[^\]]*)\]\((.[^\)]*)\)/ig;
+    match = /!{0,1}\[(.[^\]]*)\]\((.[^\)]*)\)/ig;
     txt = txt.replace(match, "<a href='$2'>$1</a>");
 
+    // headlines 1
+    match = /(\n|^)#{3} (.[^\n]+)\n/ig;
+    txt = txt.replace(match, "<h3>$2</h3>");
+
+    // headlines 2
+    match = /(\n|^)#{2} (.[^\n]+)\n/ig;
+    txt = txt.replace(match, "<h2>$2</h2>");
+
+    // headlines 3
+    match = /(\n|^)#{1} (.[^\n]+)\n/ig;
+    txt = txt.replace(match, "<h1>$2</h1>");
+
+    // bold italic
+    match = /(\n|^|\s)\*{3}(.[^\s\*]+)\*{3}\s/ig;
+    txt = txt.replace(match, " <b><i>$2</i></b> ");
+    match = /(\n|^|\s)_{3}(.[^\s_]+)_{3}\s/ig;
+    txt = txt.replace(match, " <b><i>$2</i></b> ");
+
+    // italic
+    match = /(\n|^|\s)\*(.[^\s\*]+)\*\s/ig;
+    txt = txt.replace(match, " <i>$2</i> ");
+    match = /(\n|^|\s)_(.[^\s_]+)_\s/ig;
+    txt = txt.replace(match, " <i>$2</i> ");
+
+    // bold
+    match = /(\n|^|\s)\*{2}(.[^\s\*]+)\*{2}\s/ig;
+    txt = txt.replace(match, " <b>$2</b> ");
+    match = /(\n|^|\s)_{2}(.[^\s]+)_{2}\s/ig;
+    txt = txt.replace(match, " <b>$2</b> ");
+
+
     //replace hashtags
-    match = /#(\w+)/ig;
+    match = /#([\u00C0-\u1FFF\u2C00-\uD7FF\w&]+)/ig;
     txt = txt.replace(match, "<a href='pusteblume:$1'>#$1</a>");
 
     //replace headline tags
@@ -54,8 +85,12 @@ Alloy.Globals.replaceText = function(txt) {
     match = /\n\n/ig;
     txt = txt.replace(match, "\n");
 
+
+
+
     match = /\n/ig;
     txt = txt.replace(match, "<br/>");
+
 
     match = /\(C\)/ig;
     txt = txt.replace(match, "©");
@@ -69,6 +104,12 @@ Alloy.Globals.replaceText = function(txt) {
     txt = txt.replace(match, "😞");
     match = /\:D/ig;
     txt = txt.replace(match, "😃");
+    match = /\(c\)/ig;
+    txt = txt.replace(match, "©");
+    match = /\(r\)/ig;
+    txt = txt.replace(match, "®");
+    match = /\(tm\)/ig;
+    txt = txt.replace(match, "™");
 
     match = /<3/ig;
     txt = txt.replace(match, "❤");
